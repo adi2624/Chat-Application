c75d8c22a2982c0fe6f2ddc6f406fd76bde07cfd
Aditya Rajguru <adityarajguru@Adityas-MacBook-Pro.local>
2018-07-19 18:14:32 -0500

    Search with basic UI implemented

--- app/src/main/java/com/example/adityarajguru/chat/MainActivity.java
+++ app/src/main/java/com/example/adityarajguru/chat/MainActivity.java
@@ -3,33 +3,32 @@ package com.example.adityarajguru.chat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button Users;
-    Button ChatRoom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Users = (Button)findViewById(R.id.userbutton);
-        ChatRoom = (Button)findViewById(R.id.chatroom);
-        Users.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,AddUserActivity.class);
-                startActivity(intent);
-            }
-        });
-        ChatRoom.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ChatActivity.class);
-                startActivity(intent);
-            }
-        });
+
+    }
+    @Override public boolean onCreateOptionsMenu(Menu menu) { MenuInflater menuInflater = getMenuInflater(); menuInflater.inflate(R.menu.search_menu, menu);
+        return true;
+    }
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search_icon:
+                Log.e("Search requested","fd");
+                onSearchRequested();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item); }
     }
 }
---
+++ app/src/main/java/com/example/adityarajguru/chat/SearchActivity.java
@@ -0,0 +1,70 @@
+package com.example.adityarajguru.chat;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private ListView lv;
+    DatabaseReference users;
+    List<String> usernames = new ArrayList<>();
+    List<String> results = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        lv = (ListView)findViewById(R.id.search_list);
+        Intent intent = getIntent();
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            final String query = intent.getStringExtra(SearchManager.QUERY);
+           // Toast.makeText(SearchActivity.this, query,Toast.LENGTH_SHORT).show();
+            users = FirebaseDatabase.getInstance().getReference("username");
+            ValueEventListener valueEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for(DataSnapshot ds : dataSnapshot.getChildren())
+                    {
+                        for(DataSnapshot email: ds.getChildren()){
+                            usernames.add(email.getValue().toString());
+                            Log.e("VALUE: ",email.getValue().toString());
+                        }
+                    }
+                    for(int i=0;i<usernames.size();i++)
+                    {
+                        if(usernames.get(i).equals(query))
+                        {
+                            results.add(usernames.get(i));
+                            Log.e("MATCH: ",usernames.get(i));
+                        }
+                    }
+                    lv.setAdapter(new ArrayAdapter<String>(SearchActivity.this,
+                            android.R.layout.simple_list_item_1,results));
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            };
+            users.addListenerForSingleValueEvent(valueEventListener);
+
+        }
+    }
+}
